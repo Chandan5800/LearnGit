be8e11d106f76074e1a8c1f05ab38607933875fe
Chandan Kumar <chandankumar61006br@gmail.com>
2023-09-01 21:10:49 +0530

    Adding sub file

---
+++ sub.java
@@ -0,0 +1,8 @@
+public class sub {
+
+    public static void main(String[] args) {
+        int c = 55;
+        int d = 45;
+        System.out.println("The difference of given number is " + (c - d));
+    }
+}
